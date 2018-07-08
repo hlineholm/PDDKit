ad3967e5c6cc19de50824455a625d1b44f96cd0b
Henrik Lineholm <henrik.lineholm@icloud.com>
2018-07-08 07:17:28 +0200

    Add a CocoaTouch framework

---
+++ PDDKit/PDDKit/PDDKit.h
@@ -0,0 +1,19 @@
+//
+//  PDDKit.h
+//  PDDKit
+//
+//  Created by Henrik Lineholm on 2018-07-08.
+//  Copyright © 2018 Henrik Lineholm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PDDKit.
+FOUNDATION_EXPORT double PDDKitVersionNumber;
+
+//! Project version string for PDDKit.
+FOUNDATION_EXPORT const unsigned char PDDKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PDDKit/PublicHeader.h>
+
+
